49f6b811aafcf12b38925daa3d4b580795623c15
Philipp <kg9dh@hotmail.ch>
2012-12-30 00:01:39 +0100

    complete

--- BroadCastRam/src/ch/kg9dh/main/RamUsage.java
+++ BroadCastRam/src/ch/kg9dh/main/RamUsage.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RamUsage extends JavaPlugin{
@@ -29,9 +30,11 @@ public class RamUsage extends JavaPlugin{
 		int fm = (int) free/1048576;
 		int um = (int) used/1048576;
 		
+		int percent = 100*um/tm;
+		
 		if(cmd.getName().equalsIgnoreCase("ram")){
 			Bukkit.getServer().broadcastMessage(ChatColor.YELLOW+"[Total: "+tm+"MB]"+ChatColor.RED+" [Used: "+um+"MB] "+ChatColor.GREEN+"[Free: "+fm+"MB]");
-			Bukkit.getServer().broadcastMessage(ChatColor.RED+perc(tm,um)/*+ChatColor.GREEN+perc(tm,fm)*/);
+			Bukkit.getServer().broadcastMessage(ChatColor.RED+perc(tm,um)+ChatColor.YELLOW+" ["+percent+"%] Are used.");
 		}
 		
 		return false;
@@ -39,8 +42,8 @@ public class RamUsage extends JavaPlugin{
 	
 	public String perc(int total, int part){
 		String s = ChatColor.RED+"";
-		int x = 100*part/total;
-		int z = 110-x;
+		int x = 70*part/total;
+		int z = 70-x;
 		for(int y=0; y<=x; y++){
 			s = s+"|";
 		}
